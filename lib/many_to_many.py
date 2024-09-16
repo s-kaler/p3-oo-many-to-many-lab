e3cb491996e6a4bf73ee34a69ffbcf520a316dc4
Sundeep <sundeep.kaler@gmail.com>
2024-09-16 00:43:15 -0400

    complete lab

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,51 @@
 class Author:
-    pass
+    all = []
+    def __init__(self, name):
+        self.name = name
+    
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+    
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+    
+    def total_royalties(self):
+        total = 0
+        for contract in Contract.all:
+            if contract.author == self:
+                total += contract.royalties
+        return total
 
 
 class Book:
-    pass
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+    
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if isinstance(author, Author) and isinstance(book, Book) and isinstance(date, str) and isinstance(royalties, int):
+            self.author = author
+            self.book = book
+            self.date = date
+            self.royalties = royalties
+            Contract.all.append(self)
+        else:
+            raise TypeError("Invalid types for author, book, date, and/or royalties")
+        
+    def contracts_by_date(date):
+        return [contract for contract in Contract.all if contract.date == date]
